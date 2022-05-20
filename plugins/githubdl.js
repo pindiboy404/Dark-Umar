8d5221f763a0f904712a0053ee52a4ec841d5a74
DARK UMAR <104460597+pindiboy404@users.noreply.github.com>
2022-05-20 19:42:07 +0500

    Update githubdl.js

--- plugins/githubdl.js
+++ plugins/githubdl.js
@@ -1,7 +1,7 @@
 let handler = async (m, { args, usedPrefix, command }) => {
 
-    if (!args[0]) throw 'where is the username bro?\nExample Ajmal-Achu'
-    if (!args[1]) throw 'where is the repo??\nExample Wizard-Ser'
+    if (!args[0]) throw 'where is the username bro?\nExample pindiboy404'
+    if (!args[1]) throw 'where is the repo??\nExample Dark-Umar'
     
     let url = `https://github.com/${args[0]}/${args[1]}/archive/refs/heads/main.zip`
     //ByRizkyAdi
